14ff877f8a2a5ab80140f5a6c39312bb7a5b368b
Mayank-gozu <mayank_ag@hotmail.com>
2019-02-13 18:59:46 +0530

    Final Commit day2

--- music/admin.py
+++ music/admin.py
@@ -1,6 +1,7 @@
 #admin functuionality to add delete 
 from django.contrib import admin
-from .models import Album
+from .models import Album, song
 # Register your models here.
 
 admin.site.register(Album)
+admin.site.register(song)
